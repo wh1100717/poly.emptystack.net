760cb7e4dba32f56021b91fbe53cb3e9fdf06797
Martin0809 <maning0809@gmail.com>
2014-04-28 08:27:20 +0800

    add other browser style

--- js/mm.js
+++ js/mm.js
@@ -1,23 +1,8 @@
 $(document).ready(function(){
-
-setInterval(function(){
-
-
-$("#head").css("-webkit-transform","rotate(-10deg)");
-
-
-setTimeout(function(){
-
-
-$("#head").css("-webkit-transform","rotate(10deg)");
-
-},1000);
-
-},2000);
-
-
-
-
-
-
+	setInterval(function(){
+		$("#head").css("-webkit-transform","rotate(-10deg)");
+		setTimeout(function(){
+			$("#head").css("-webkit-transform","rotate(10deg)");
+		},1000);
+	},2000);
 });
